06bd6a28d4c3ff1a429237126f8155962cc73adc
Riiccardob <riccardobarone1704@gmail.com>
2026-02-20 12:36:24 +0100

    refactoring

--- srcNF/live_sniffer/config.py
+++ srcNF/live_sniffer/config.py
@@ -109,10 +109,10 @@ FLOW_EXPIRATION_CHECK_INTERVAL: int = 10
 
 MODEL_TYPE: str = "xgboost"
 MODEL_PATH: Path = MODELS_DIR / MODEL_TYPE / "model.pkl"
-ATTACK_THRESHOLD: float = 0.75
+ATTACK_THRESHOLD: float = 0.5
 INFERENCE_BATCH_SIZE: int = 100
-# INFERENCE_BATCH_TIMEOUT: float = 5.0
-INFERENCE_BATCH_TIMEOUT: float = 2.0
+INFERENCE_BATCH_TIMEOUT: float = 5.0
+# INFERENCE_BATCH_TIMEOUT: float = 2.0
 
 # ============================================================================
 # OPERATION
